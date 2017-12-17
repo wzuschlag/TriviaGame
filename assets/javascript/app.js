232852a80bb57e7b54c1710fc57acb021fbd33c2
Walter Zuschlag <walterzuschlag@Walters-MacBook-Pro.local>
2017-12-17 15:27:34 -0600

    added gif & start .js HMWK5.2

---
+++ assets/javascript/app.js
@@ -0,0 +1,53 @@
+
+//Questions
+var questions = [{
+  question: "What was John Candy's character's name in the 1987 comedy movie, Planes, Trains, and Automobiles?",
+  choices: ["Del Griffith", "Don Griffith", "Dil Griffith", "Dom Griffith"],
+  answer: "Del Griffith",
+  image:"assets/images/JohnCandy.gif"
+}, {
+  question: "Bruce Banner turns into what fictional superhero when he becomes angry?",
+  choices: ["Spiderman", "Batman", "Hulk", "Wonder Woman"],
+  answer: "The Hulk",
+  image:"assets/images/TheHulk.gif"
+}, {
+  question: "Which President is on the United States 1,000 dollar bill?",
+  choices: ["William McKinley", "Grover Cleveland", "Benjamin Harrison", "Ulysses S. Grant"],
+  answer: "Grover Cleveland",
+  image:"assets/images/money.gif"
+}, {
+  question: "What football team had the biggest scoring comeback in the history of the Super Bowl?",
+  choices: ["Green Bay", "Vikings", "Cowboys", "Patriots"],
+  answer: "Patriots",
+  image:"assets/images/patriots.gif"
+}, {
+  question: "Traditionally, the term 'caviar' refers to the salt-cured roe of which fish?",
+  choices: ["Salmon", "Cod", "Albacore", "Sturgeon"],
+  answer: "Sturgeon",
+  image:"assets/images/sturgeon.gif"
+}, {
+  question: "What chemical element gives the blood of a lobster a bluish tint?",
+  choices: ["Copper", "Zinc", "Gold", "Lead"],
+  answer: "Copper",
+  image:"assets/images/lobster.gif"
+}, {
+  question: "In humans, what is the only internal organ capable of regenerating lost tissue?",
+  choices: ["Liver", "Kidney", "Spleen", "Pancreas"],
+  answer: "Liver",
+  image:"assets/images/liver.gif"
+}, {
+  question: "What is the most popular breed of dog in the United States?",
+  choices: ["German Shepherd", "Great Dane", "Labrador Retriever", "Poodle"],
+  answer: "Labrador Retriever",
+  image:"assets/images/labrador.gif"
+},{
+  question: "Which is the most abundant metal in the earth's crust?",
+  choices: ["Silver", "Aluminum", "Lead", "Iron"],
+  answer: "Aluminum",
+  image:"assets/images/aluminum.gif"
+},{
+  question: "How many castaways were there on the American sitcom Gilligan's Island?",
+  choices: ["Nine", "Eight", "Seven", "Six"],
+  answer: "Seven",
+  image:"assets/images/gilligansisland.gif"
+}];
